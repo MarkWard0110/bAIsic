ef6fbe70796a1612f9bea6f6045d21567f1c71ea
Mark Ward <mark.ward@binaryward.com>
2025-01-17 08:09:56 -0600

    add amazon bedrock runtime

---
+++ src/BAIsic.Interlocutor.AmazonBedrock/AmazonBedrockOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAIsic.Interlocutor.AmazonBedrock
+{
+    public class AmazonBedrockOptions
+    {
+        public RequestOptions? RequestOptions { get; set; } = null;
+        public string? ResponseFormat { get; set; } = null;
+    }
+}
